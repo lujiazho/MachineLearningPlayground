87f5d2d3c32a96d7c5a944db70b7eb2718d458fb
lujiazho <lujiazho@usc.edu>
2022-05-26 22:53:29 -0700

    add files

--- miniml/denestimation/nonparametric/KDE.py
+++ miniml/denestimation/nonparametric/KDE.py
@@ -1,3 +1,13 @@
+#########################################
+#         MLPlayground for Numpy        #
+#      Machine Learning Techniques      #
+#                 v1.0.0                #
+#                                       #
+#         Written by Lujia Zhong        #
+#       https://lujiazho.github.io/     #
+#              26 May 2022              #
+#              MIT License              #
+#########################################
 import numpy as np
 
 class KDE:
