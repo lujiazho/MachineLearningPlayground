8287ea9b37cfb4bd4ef21d53c0671972bad8ad42
Harry <68451409+lujiazho@users.noreply.github.com>
2022-05-26 22:45:35 -0700

    Update __init__.py

---
+++ miniml/__init__.py
@@ -0,0 +1,10 @@
+#########################################
+#         MLPlayground for Numpy        #
+#      Machine Learning Techniques      #
+#                 v1.0.0                #
+#                                       #
+#         Written by Lujia Zhong        #
+#       https://lujiazho.github.io/     #
+#              26 May 2022              #
+#              MIT License              #
+#########################################
